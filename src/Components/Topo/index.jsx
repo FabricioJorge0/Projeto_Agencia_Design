83ad2a82ac03e41dcce50e5ec628a0bdca962115
Fabricio Jorge <fabricioj.dev@gmail.com>
2024-09-24 00:50:46 -0400

    criando e estilizando componente header

--- src/Components/Topo/index.jsx
+++ src/Components/Topo/index.jsx
@@ -8,15 +8,15 @@ import Image from 'next/image'
 
 export default function Topo(props){
     return(
-        <div>
+        <header className={props.ehTemaEscuro ? style.containerHeaderEscuro : style.containerHeaderClaro}>
+  
+            <Image className={style.imgLogo} src={Logo} alt='Logo'/>
+
             <div>
-                <Image src={Logo} alt='Logo'/>
-            </div>
-            <div>
-                <button onClick={props.acao_onclick} className={props.ehTemaEscuro}>
+                <button onClick={props.acao_onclick} className={props.ehTemaEscuro ? style.btnTemaEscuro : style.btnTemaClaro}>
                     <Image className={style.iconesBtn} src={props.ehTemaEscuro ? Sun : Moon} alt='Icone'/>
                 </button>
             </div>
-        </div>
+        </header>
     )
 }
