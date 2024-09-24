a5afac909097436ff4e50ed5c46f6081ababd121
Fabricio Jorge <fabricioj.dev@gmail.com>
2024-09-24 01:01:08 -0400

    corrigindo bug de css do header

--- src/Components/Topo/index.jsx
+++ src/Components/Topo/index.jsx
@@ -9,13 +9,14 @@ import Image from 'next/image'
 export default function Topo(props){
     return(
         <header className={props.ehTemaEscuro ? style.containerHeaderEscuro : style.containerHeaderClaro}>
-  
-            <Image className={style.imgLogo} src={Logo} alt='Logo'/>
+            <div className={style.cabecalho}>
+                <Image className={style.imgLogo} src={Logo} alt='Logo'/>
 
-            <div>
-                <button onClick={props.acao_onclick} className={props.ehTemaEscuro ? style.btnTemaEscuro : style.btnTemaClaro}>
-                    <Image className={style.iconesBtn} src={props.ehTemaEscuro ? Sun : Moon} alt='Icone'/>
-                </button>
+                <div>
+                    <button onClick={props.acao_onclick} className={props.ehTemaEscuro ? style.btnTemaEscuro : style.btnTemaClaro}>
+                        <Image className={style.iconesBtn} src={props.ehTemaEscuro ? Sun : Moon} alt='Icone'/>
+                    </button>
+                </div>
             </div>
         </header>
     )
